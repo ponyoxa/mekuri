e871dba2fc3fd32780a857042c671ca1970e181c
ponyoxa <kmsm805@gmail.com>
2025-02-24 21:21:04 +0900

    インデント修正

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ export interface Env {
 	DISCORD_GUILD_ID: string
 }
 
-
 export default {
 	async scheduled (
 		controller:ScheduledController,
@@ -11,20 +10,19 @@ export default {
 		ctx:ExecutionContext,
 	){
 		switch(controller.cron){
-		case"0 20 * * tue":
-			await startMessage()
-			break
-		case"45 20 * * tue":
-			await summaryMessage()
-			break
-		case"0 21 * * tue":
-			await finishMessage()
-			break
+			case"0 20 * * tue":
+				await startMessage()
+				break
+			case"45 20 * * tue":
+				await summaryMessage()
+				break
+			case"0 21 * * tue":
+				await finishMessage()
+				break
 		}
 		console.log("cron processed")
 	},
 }
-
 
 function startMessage() {
 	throw new Error("Function not implemented.")
